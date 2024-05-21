9fc6239938a36141ccebd30ac19b552628a4b88b
manoj_bhosale <manoj.bhosale@motifworks.com>
2024-05-21 18:56:05 +0530

    initial commit

---
+++ src/main/java/com/library/myspringdemo/MySpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.library.myspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringDemoApplication.class, args);
+    }
+
+}
